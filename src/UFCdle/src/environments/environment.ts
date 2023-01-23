c4c32d5760d7f8c26995d45769b8069dc62b82c9
DanielMc56 <daniel.mccartney56@mail.dcu.ie>
2023-01-23 19:04:21 +0000

    added styling to login page

--- src/UFCdle/src/environments/environment.ts
+++ src/UFCdle/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBCIFyiYPy7EGSoSfVdW9Pi9aG77LMyYoI",
+    authDomain: "ufcdle.firebaseapp.com",
+    projectId: "ufcdle",
+    storageBucket: "ufcdle.appspot.com",
+    messagingSenderId: "304029307562",
+    appId: "1:304029307562:web:bffe31f64da292cf7f36d3",
+    measurementId: "G-GHE0WMWRN6"
+  }
 };
 
 /*

6b854109c5cc0af4f249e38fa01e4ce95d847ffb
Daniel Mc Cartney <daniel.mccartney56@mail.dcu.ie>
2023-01-31 16:45:04 +0000

    Daniel 3101

---
+++ src/UFCdle/src/app/board/board.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import fighters from '../../db.json';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+})
+export class BoardComponent implements OnInit {
+  list = fighters;
+  searchModeOption = 'contains';
+  searchExprOption: any = 'fighterName';
+  searchTimeoutOption = 200;
+  minSearchLengthOption = 1;
+  showDataBeforeSearchOption = false;
+  currentFighter: any = {};
+
+  constructor() {}
+
+  onValueChanged(e: any) {
+  this.currentFighter = e.itemData
+  console.log(this.currentFighter);
+  
+  
+  }
+
+  ngOnInit(): void {
+    for (let i = 0; i < this.list.fighters.length; i++) {
+      console.log(this.list.fighters[i]);
+    }
+  }
+}
